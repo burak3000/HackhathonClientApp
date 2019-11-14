625df03fdd33eb9e512d5655eb39360a1c2d91ef
cmsmy <ecemsomaygmail.com>
2019-11-14 21:17:33 +0300

    Added cardView

--- app/src/main/java/com/example/hackhathonclientapp/MainActivity.java
+++ app/src/main/java/com/example/hackhathonclientapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -34,10 +35,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnConnect = findViewById(R.id.btnAddCard);
-
-
+        Button btnAdd = findViewById(R.id.btnAddCard);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.card1).setVisibility(View.VISIBLE);
+            }
+        });
     }
+
 
     private PrintWriter output;
     private BufferedReader input;
